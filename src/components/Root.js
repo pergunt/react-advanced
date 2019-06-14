4f4a6915436c9ff40bedd8e6aebf2b38bbfdcf03
iroskoshnyi <ivan.roskoshnyi@chromeriver.com>
2019-06-14 17:16:29 +0300

    init commit

---
+++ src/components/Root.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+/**
+ * @class Root
+ * @extends Component
+ */
+class Root extends Component {
+  /**
+   * @returns {*}
+   */
+  render() {
+    return (
+      <div>
+        root
+      </div>
+    );
+  }
+}
+export default Root;
